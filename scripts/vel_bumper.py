941e7246db5925731eb7ea10ba3efb47af741902
Teppei Kanayama <1994teppei0615kanayama@gmail.com>
2015-10-09 20:39:16 +0900

    get_param

--- scripts/vel_bumper.py
+++ scripts/vel_bumper.py
@@ -3,8 +3,10 @@ import rospy
 from geometry_msgs.msg import Twist
 from kobuki_msgs.msg import BumperEvent
 
-rospy.init_node('vel_publisher')
-pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size = 10)
+rospy.init_node('vel_bumper')
+vel_x = rospy.get_param('~vel_x', 0.5)
+vel_rot = rospy.get_param('~vel_rot', 1.0)
+pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size = 1)
 
 def callback(bumper):
     print bumper
@@ -18,13 +20,13 @@ while not rospy.is_shutdown():
     vel = Twist()
     direction = raw_input('f, b, l, r, q: ')
     if 'f' in direction:
-        vel.linear.x = 0.5
+        vel.linear.x = vel_x
     if 'b' in direction:
-        vel.linear.x = -0.5
+        vel.linear.x = vel_x * (-1)
     if 'l' in direction:
-        vel.angular.z = 1.0
+        vel.angular.z = vel_rot
     if 'r' in direction:
-        vel.angular.z = -1.0
+        vel.angular.z = vel_rot * (-1)
     if 'q' in direction:
         break
     print vel
